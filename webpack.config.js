a322b2ec323c542b3d87e3338672298d69bc5aa0
Leon Ho <vuonghominh@gmail.com>
2016-05-07 15:43:59 +0700

    Configuring Webpack Dev Server and React Hot loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,30 @@
 var path = require("path");
+var webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+    './src/index' // Your appʼs entry point
+  ],
   output: {
     path: path.resolve(__dirname, './build'),
+    publicPath: '/build/',
     filename: "bundle.js"
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ['babel']
+        loaders: ['react-hot', 'babel']
       }
     ]
   }
