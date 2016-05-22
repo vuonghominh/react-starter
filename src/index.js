7dc53db0977e1ef3f7852f185afd5b8eb9068300
Leon Ho <vuonghominh@gmail.com>
2016-05-22 16:53:20 +0700

    config store

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
-import reduxThunk from 'redux-thunk';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import configureStore from './store/configureStore';
 
 import App from './components/App/App';
 import Signin from './components/Auth/Signin';
-import Signout from './components/auth/Signout';
+import Signout from './components/Auth/Signout';
+import ProjectListContainer from './components/ProjectListContainer/ProjectListContainer';
 import RequireAuth from './components/RequireAuthentication';
 import reducers from './reducers';
 import { AUTH_USER } from './actions/types';
 
-const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
-const store = createStoreWithMiddleware(reducers)
+const store = configureStore();
 
 const token = localStorage.getItem('token');
+
 if (token) {
   store.dispatch({
     type: AUTH_USER,
@@ -28,6 +28,7 @@ render(
       <Route path="/signin" component={Signin}></Route>
       <Route path="/" component={RequireAuth(App)}>
         <Route path="signout" component={Signout}></Route>
+        <Route path="projects" component={ProjectListContainer}></Route>
       </Route>
     </Router>
   </Provider>
---
+++ src/store/configureStore.jsx
@@ -0,0 +1,9 @@
+import { compose, createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+import reducers from '../reducers';
+
+const buildStore = compose(applyMiddleware(reduxThunk))(createStore);
+
+export default function configureStore(initialState) {
+  return buildStore(reducers, initialState);
+}
